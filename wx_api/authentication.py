496f8e912ac4793686bec9f27d101fab0d2d6f61
xuqil <13829842679@163.com>
2019-03-28 18:46:36 +0800

    添加状态码

--- wx_api/authentication.py
+++ wx_api/authentication.py
@@ -11,27 +11,54 @@ class MyAuthentication(View):
     def dispatch(self, request, *args, **kwargs):
         authentication = request.META.get('HTTP_AUTHENTICATION')  # 获取头部信息
         if authentication is None or authentication == '':
-            return HttpResponse("error")
+            # authentication不存在
+            return HttpResponse({
+                'code': 406,
+                'msg': 'authentication is None!'
+            })
         try:
             base64_authentication = str(authentication).split(' ')
             base64_token = base64_authentication[1]
         except IndexError:
-            return HttpResponse("格式不符合")
+            # authentication格式错误
+            return HttpResponse({
+                'code': 400,
+                'msg': 'Bad Request!'
+            })
         try:
             request_token = str(base64_decode(base64_token)).split(":")[1]
             uid = str(base64_decode(base64_token)).split(":")[2]
         except IndexError:
-            return HttpResponse("authentication格式不正确")
+            # authentication格式错误
+            return HttpResponse({
+                'code': 400,
+                'msg': 'Bad Request!'
+            })
         except ValueError:
-            return HttpResponse("authentication格式不正确")
+            return HttpResponse({
+                'code': 400,
+                'msg': 'Bad Request!'
+            })
         try:
             user = AuthToken.objects.filter(uid_id=uid).first()
         except ValueError:
-            return HttpResponse("userid的值不对，验证失败")
+            # userid的值不对，验证失败
+            return HttpResponse({
+                'code': 403,
+                'msg': 'Forbidden!'
+            })
         if user is None:
-            return HttpResponse("找不到该用户，验证失败")
+            # 找不到该用户，验证失败
+            return HttpResponse({
+                'code': 403,
+                'msg': 'Forbidden!'
+            })
         token = user.key
         if token != request_token:
-            return HttpResponse("token不正确或者已经过期")
+            # token不正确或者已经过期
+            return HttpResponse({
+                'code': 403,
+                'msg': 'Forbidden!'
+            })
         handler = super(MyAuthentication, self).dispatch(request, *args, **kwargs)
         return handler
