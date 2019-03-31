029d437b533a71ef3042bc0090cff9fef0c23bfb
xuqil <13829842679@163.com>
2019-03-31 22:50:17 +0800

    封装查询(使用反射)

--- wx_api/views.py
+++ wx_api/views.py
@@ -212,13 +212,13 @@ class ReceiveImages(MyAuthentication):
         else:
             if (gid is None and g_sid is not None) or (gid is None and g_sid is None):
                 # gid is not None时该查询没有对某组下的索引进行查询，默认不分组查询
-                car_image = CarImage.objects.all().filter(uid_id=uid).order_by('created')
+                car_image = CarObj.objects.all().filter(uid_id=uid).order_by('created')
             elif gid is not None and g_sid is None:
                 # 只查询组
-                car_image = CarImage.objects.all().filter(uid_id=uid).filter(gid=gid).order_by('created')
+                car_image = CarObj.objects.all().filter(uid_id=uid).filter(gid=gid).order_by('created')
             elif gid is not None and g_sid is not None:
                 # 具体查询
-                car_image = CarImage.objects.all().filter(uid_id=uid).filter(gid=gid). \
+                car_image = CarObj.objects.all().filter(uid_id=uid).filter(gid=gid). \
                     filter(g_sid=g_sid).order_by('created')
         return car_image
 
