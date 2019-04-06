910a51868264a6d6c7744c9ed2a9cb694af70623
xuqil <13829842679@163.com>
2019-04-06 21:39:59 +0800

    url删除后缀/

--- wx_api/urls.py
+++ wx_api/urls.py
@@ -6,12 +6,12 @@ from django.conf import settings
 app_name = 'wx_api'
 
 urlpatterns = [
-    path('token/', views.acquire_token, name='token'),
-    path('address/2/', views.CheckToken.as_view(), name='check_token'),
-    path('car/data/', views.ReceiveData.as_view(), name='receive_data'),
-    path('car/image/', views.ReceiveImages.as_view(), name='receive_images'),
-    path('car/', views.GetData.as_view(), name='get_data'),
-    path('user/', views.GetList.as_view(), name='get_list'),
-    path('car/<str:type_>/<str:uid>/', views.DeleteCar.as_view(), name='delete_car'),
+    path('token', views.acquire_token, name='token'),
+    path('address/2', views.CheckToken.as_view(), name='check_token'),
+    path('car/data', views.ReceiveData.as_view(), name='receive_data'),
+    path('car/image', views.ReceiveImages.as_view(), name='receive_images'),
+    path('car', views.GetData.as_view(), name='get_data'),
+    path('user', views.GetList.as_view(), name='get_list'),
+    path('car/<str:type_>/<str:uid>', views.DeleteCar.as_view(), name='delete_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # 图片访问url:http://127.0.0.1:8000/v1/images_upload/2_29_1_1554037928.png
